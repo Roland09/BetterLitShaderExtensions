8c0a04c1f07c7d954e9cfb30e60f87bb860f4ba7
Roland09 <roland.cidar@gmail.com>
2021-05-20 07:38:13 +0200

    Manual conversion

--- Assets/BetterLitShaderExtensions/MaterialUpgrader/Editor/Material/HDLitToBetterLitMaterialUpgrader.cs
+++ Assets/BetterLitShaderExtensions/MaterialUpgrader/Editor/Material/HDLitToBetterLitMaterialUpgrader.cs
@@ -15,6 +15,33 @@ namespace Rowlan.BetterLitShaderExtensions.MaterialUpgrader
             dstMaterial.hideFlags = HideFlags.DontUnloadUnusedAsset;
 
             base.Convert(srcMaterial, dstMaterial);
+
+			#region Mapping of material properties
+
+			// perform manual conversion. BetterLit Shaders would support automatic conversion when you click on a material,
+			// but if you have multiple materials, you'd have to click on every single one of them.
+			// => we do manual conversion
+
+			Texture mainTex = srcMaterial.GetTexture("_MainTex");
+            if (mainTex != null) {
+                dstMaterial.SetTexture("_AlbedoMap", mainTex);
+            }
+
+            Color baseColor = srcMaterial.GetColor("_BaseColor");
+            if( baseColor != null) {
+                dstMaterial.SetColor("_Tint", baseColor);
+			}
+
+            Texture bumpMap = srcMaterial.GetTexture("_BumpMap");
+            if (bumpMap != null)
+            {
+                dstMaterial.SetTexture("_NormalMap", bumpMap);
+            }
+
+            #endregion Mapping of material properties
+
+            MaterialEditor.ApplyMaterialPropertyDrawers(dstMaterial);
+            EditorUtility.SetDirty(dstMaterial);
         }
     }
 }
